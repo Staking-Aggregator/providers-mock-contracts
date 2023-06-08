e1b7000589028ccb364b819e95d2f2e558c7a4e2
rishabhraghwendra@18 <rishabhraghwendra2002@gmail.com>
2023-06-08 20:48:09 +0530

    added redirect to staking page and binded mock smart contracts

--- contracts/Lido/stETH.sol
+++ contracts/Lido/stETH.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract stETH is ERC20 {
-    constructor() ERC20("Lido stETH", "stETH") {}
+    constructor() ERC20("Lido stETH", "wstETH") {}
 
     function mint(address _to,uint _amount) public {
         _mint(_to, _amount);
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,16 +3,22 @@ require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 const NODE_PROVIDER = process.env.QUICK_NODE_PROVIDER;
+const MUMBAI_PROVIDER = process.env.QUICK_NODE_MUMBAI;
 const PRIVATE_KEYS = process.env.PRIVATE_KEY;
 const ETHERSCAN_API = process.env.ETHERSCAN_API;
+const POLYGON_API = process.env.POLYGON_API;
 module.exports = {
   solidity: "0.8.0",
   etherscan: {
-    apiKey: ETHERSCAN_API,
+    apiKey: POLYGON_API,
   },
   networks: {
     sepolia: {
       url: NODE_PROVIDER,
+      accounts: [PRIVATE_KEYS]
+    },
+    mumbai: {
+      url: MUMBAI_PROVIDER,
       accounts: [PRIVATE_KEYS]
     }
   }
